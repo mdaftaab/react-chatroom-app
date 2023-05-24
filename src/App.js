734f6998564e00ad1eabbd5e324b4585bfb64b23
Md Aftaab <md.yash99@gmail.com>
2023-05-24 18:58:43 +0530

    edit app and css file

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState("Dammy User");
+  const [chat setChat] = useState([{name: user1, message:"hi"}]);
   return (
     <div>
-      <h1>User: {name}</h1>
+      <h3>User: {name}</h3>
       <div className='chat-container'>
-        <div className='container'>
+
+        {<div className='container me'>
         <p className='chatbox'>
           <strong>name: </strong>
           <span>chat message</span>
         </p>
-        </div>
-        <div className='container me'>
-        <p className='chatbox'>
-          <strong>name: </strong>
-          <span>chat message</span>
-        </p>
-        </div>
+        </div>}
+        
+      </div>
+      <div className='btm'>
+        <input type="text" placeholder='enter your chat'></input>
+        <button>Send</button>
       </div>
     </div>
   );
